060fb371a55f2069326d83fabbf7cd1148d10c22
Bin Matsui <matsui-bin@codeer.co.jp>
2016-08-10 09:05:07 +0900

    初期設定

---
+++ PokemonGoGetLog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokemonGoGetLog.MvcApplication" Language="C#" %>
---
+++ PokemonGoGetLog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokemonGoGetLog.Startup))]
+namespace PokemonGoGetLog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
